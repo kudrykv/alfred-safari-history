374eced719e651c4f1e9b6c22efe9fe258f2c65a
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-06-08 15:50:13 +0300

    need CGO_ENABLED for binary to work with sqlite3

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ _goarches=(amd64)
 
 for _goos in "${_gooses[@]}"; do
   for _goarch in "${_goarches[@]}"; do
-    GOOS=${_goos} GOARCH=${_goarch} CGO_ENABLED=0 go build -o run ./app
+    GOOS=${_goos} GOARCH=${_goarch} go build -o run ./app
 
     _zipname="Safari_History_${_version}_${_goos}_${_goarch}.alfredworkflow"
     zip -r "${_zipname}" icon.png info.plist preflight.sh run
