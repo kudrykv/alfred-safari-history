fbf080ab4a92fe8e63852a1ba4e779609b50c778
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-06-08 15:49:24 +0300

    releaser

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+_version=$(grep -A 1 version info.plist | tail -n 1 | perl -pe 's/.*(\d+\.\d+\.\d+).*/v\1/')
+
+_gooses=(darwin)
+_goarches=(amd64)
+
+for _goos in "${_gooses[@]}"; do
+  for _goarch in "${_goarches[@]}"; do
+    GOOS=${_goos} GOARCH=${_goarch} CGO_ENABLED=0 go build -o run ./app
+
+    _zipname="Safari_History_${_version}_${_goos}_${_goarch}.alfredworkflow"
+    zip -r "${_zipname}" icon.png info.plist preflight.sh run
+  done
+done
